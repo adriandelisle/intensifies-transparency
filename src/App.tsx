0ec291d21d24fe4d2e7970bc9f9a6ee2491f2151
Adrian De Lisle <adriandelisle@gmail.com>
2019-10-07 23:19:39 -0700

    Added selecting an image to intensify

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, FunctionComponent } from "react"
+import logo from "./logo.svg"
+import "./App.css"
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface GifSelectorProps {
+  onFileSelected: (files: FileList | null) => void
 }
 
-export default App;
+const GifSelector: FunctionComponent<GifSelectorProps> = ({
+  onFileSelected
+}: GifSelectorProps) => (
+  <div className="gifSelector">
+    <label htmlFor="GifSelector">Select an image to intensify</label>
+    <input
+      type="file"
+      accept="image/*"
+      onChange={e => onFileSelected(e.target.files)}
+      name="GifSelector"
+      id="GifSelector"
+    />
+  </div>
+)
+
+interface PreviewImageProps {
+  url: string
+}
+
+const PreviewImage: FunctionComponent<PreviewImageProps> = ({
+  url
+}: PreviewImageProps) => (
+  <img src={url} className="App-logo" alt="Preview of uploaded image" />
+)
+
+const getFileUrl: (file: File) => string = (file: File) =>
+  URL.createObjectURL(file)
+
+interface AppState {
+  selectedImage: File | null
+}
+
+interface AppProps {}
+
+class App extends Component<AppProps, AppState> {
+  public readonly state: Readonly<AppState> = {
+    selectedImage: null
+  }
+
+  onImageSelected = (files: FileList | null) => {
+    if (files) {
+      this.setState({ selectedImage: files.item(0) })
+    }
+  }
+
+  render() {
+    const { selectedImage } = this.state
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <PreviewImage
+            url={selectedImage ? getFileUrl(selectedImage) : logo}
+          />
+          <GifSelector onFileSelected={this.onImageSelected} />
+        </header>
+      </div>
+    )
+  }
+}
+
+export default App
