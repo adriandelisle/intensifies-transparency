70d788cda084782b5d4a3967cdae60e8ef6aee19
Adrian De Lisle <adriandelisle@gmail.com>
2020-01-21 21:23:14 -0800

    Added a checked background to the image preview to show transparency

--- src/components/image-preview.tsx
+++ src/components/image-preview.tsx
@@ -1,4 +1,21 @@
 import React, { FunctionComponent } from 'react'
+import styled from 'styled-components'
+
+const ImagePreviewContainer = styled.div`
+  margin: 10px;
+`
+
+const CheckeredBackground = styled.div`
+  background-image: /* tint image */ linear-gradient(
+      to right,
+      rgba(192, 192, 192, 0.75),
+      rgba(192, 192, 192, 0.75)
+    ),
+    /* checkered effect */ linear-gradient(to right, black 50%, white 50%),
+    linear-gradient(to bottom, black 50%, white 50%);
+  background-blend-mode: normal, difference, normal;
+  background-size: 2em 2em;
+`
 
 interface ImagePreviewProps {
   url: string
@@ -6,4 +23,10 @@ interface ImagePreviewProps {
 
 export const ImagePreview: FunctionComponent<ImagePreviewProps> = ({
   url,
-}: ImagePreviewProps) => <img src={url} alt="Preview of uploaded file" />
+}: ImagePreviewProps) => (
+  <ImagePreviewContainer>
+    <CheckeredBackground>
+      <img src={url} alt="Preview of uploaded file" />
+    </CheckeredBackground>
+  </ImagePreviewContainer>
+)
