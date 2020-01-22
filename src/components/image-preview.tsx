4c64a26c87290c8129665654091d591c5b3d2794
Adrian De Lisle <adriandelisle@gmail.com>
2020-01-21 21:30:09 -0800

    Cleaned up the image preview component

--- src/components/image-preview.tsx
+++ src/components/image-preview.tsx
@@ -1,8 +1,17 @@
 import React, { FunctionComponent } from 'react'
 import styled from 'styled-components'
 
-const ImagePreviewContainer = styled.div`
+const ImagePreviewContainer = styled.img`
   margin: 10px;
+  background-image: /* tint image */ linear-gradient(
+      to right,
+      rgba(192, 192, 192, 0.75),
+      rgba(192, 192, 192, 0.75)
+    ),
+    /* checkered effect */ linear-gradient(to right, black 50%, white 50%),
+    linear-gradient(to bottom, black 50%, white 50%);
+  background-blend-mode: normal, difference, normal;
+  background-size: 2em 2em;
 `
 
 const CheckeredBackground = styled.div`
@@ -24,9 +33,5 @@ interface ImagePreviewProps {
 export const ImagePreview: FunctionComponent<ImagePreviewProps> = ({
   url,
 }: ImagePreviewProps) => (
-  <ImagePreviewContainer>
-    <CheckeredBackground>
-      <img src={url} alt="Preview of uploaded file" />
-    </CheckeredBackground>
-  </ImagePreviewContainer>
+  <ImagePreviewContainer src={url} alt="Preview of uploaded file" />
 )
